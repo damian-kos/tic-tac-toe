30e77e13abf4ba7ee5d891b42b8d24dba58f606d
damian-kos <pythondamian@gmail.com>
2022-11-13 12:46:42 +0000

    Draw conlcuding, clean code

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-# row list is used to define a playboard and points are assigned to these fields. Once a player reaches 15 points it means that he won.
-row = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-points = [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+from itertools import combinations
+
+row = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+points = [2, 7, 6, 9, 5, 1, 4, 3, 8]
 
 # Initializes a board and is updated later on to show where a sign was put in. 
 def current_board():
     string = ""
-    for j in range(3):
-        for i in range(3):
-            string +=  f"|{row[j][i]}"
-        string += "|\n" + "--+-+--\n"
+    for j in range(9):
+        string +=  f"|{row[j]}"
+        if j == 2 or j == 5 or j == 8 :
+            string += "|\n" + "--+-+--\n"
     return string
 
 # Check for correct inputs. Prevents signs to be overwritten.
-def input_is_not_correct(x, y):
-    if  int(x) not in {1, 2 ,3} or  int(y) not in {1, 2, 3}:
-        print("Your input is incorrect, try again. It should be 'X Y' (eg. 1 2).")
+def input_is_not_correct(x):
+    if  int(x) not in points:
+        print("Your input is incorrect, try again. It should be 'X'.Range (1-9). ")
         return True
-    if row[int(x)-1][int(y)-1] == "X" or  row[int(x)-1][int(y)-1] == "O":
-        print("This spot is already filled. Please choose other one")
+    if row[int(x)-1] != " ":
+        print("This spot is already filled. Please choose other one. ")
         return True
 
 # Defines which players turn is it.
 def where_to_input_sign(player):
     # Ask for current player's sign:
-    x, y = input(f"Where do you want to put your {player}:").strip().split(" ")
-    return int(x), int(y)
+    x = input(f"Where do you want to put your {player}. Range(1-9): ")
+    return (x)
 
 
 # Should be cleaned up. Game flow is going through here.
-def input_x_or_o():
+def tic_tac_toe():
 
     # Players' collected points
     points_x_lst = []
@@ -43,11 +45,11 @@ def input_x_or_o():
             player = "X"
 
             # Asks wgere to put "O" sign
-            x, y = where_to_input_sign(player)
-            sign_on_board(x, y, player)
+            x = where_to_input_sign(player)
+            sign_on_board(x, player)
 
             # Appends player's collected points.
-            points_x_lst.append(points[int(x)-1][int(y)-1])
+            points_x_lst.append(points[int(x)-1])
             if check_score(points_x_lst, player):
                 return True
 
@@ -59,38 +61,36 @@ def input_x_or_o():
             player = "O"
 
             # Asks wgere to put "O" sign
-            x, y = where_to_input_sign(player) 
-            sign_on_board(x, y, player)
-            
+            x = where_to_input_sign(player) 
+            sign_on_board(x, player)
+
             # Appends player's collected points.
-            points_o_lst.append(points[int(x)-1][int(y)-1])
+            points_o_lst.append(points[int(x)-1])
             if check_score(points_o_lst, player):
                 return True
 
 
 # Put sign onto the board.
-def sign_on_board(x, y, player):
-    while input_is_not_correct(x, y):
-        x, y = where_to_input_sign(player)
-    row[int(x)-1][int(y)-1] = player
+def sign_on_board(x, player):
+    while input_is_not_correct(x):
+        x= where_to_input_sign(player)
+    row[int(x)-1] = player
 
 # Draws can't be determined yet.
 def check_score(point_list, player):
     print(current_board())
     if len(point_list) >=3 :
-        for number in point_list:
-            total_points = sum(point_list)
-            if total_points == 15:
-                print(current_board())
-                print(f"Player with {player} won!")
-                return True
-            if total_points - number == 15:
-                print(current_board())
-                print(f"Player with {player} won!")
-                return True
+        # Goes through possible combinations of points in threes. And sums them up to see if they add up to 15. If they do. Player wins.
+        if any(sum(comb)==15 for comb in combinations(point_list, 3)):
+            print(f"Player with {player} won!")
+            return True
+        # If game is not resolved untill board is filled it is a draw.
+        if len(point_list) >=5 :
+            print(f"It's a draw!")
+            return True
 
 
-input_x_or_o()
+tic_tac_toe()
 
 
 
